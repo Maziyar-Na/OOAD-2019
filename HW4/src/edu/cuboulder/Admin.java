3b8a0a028198bc9a54fe11b8189cc681e0587185
Dwight J. Browne <djb@engr2gw-64-239-dhcp.int.colorado.edu>
2019-04-02 09:31:02 -0600

    Additional Classes

---
+++ HW4/src/edu/cuboulder/Admin.java
@@ -0,0 +1,16 @@
+package edu.cuboulder;
+
+public class Admin extends Employee {
+    Integer manage(){
+        System.out.println("placeholder for manage method");
+        return 0;
+    }
+    Integer addPatron(Patron x){
+        System.out.println("Placeholder for addPatron");
+        return 0;
+    }
+    Integer deletePatron(Patron x){
+        System.out.println("Placeholder for deletePatron");
+        return 0;
+    }
+}
---
+++ HW4/src/edu/cuboulder/Librarian.java
@@ -0,0 +1,8 @@
+package edu.cuboulder;
+
+public class Librarian extends Employee {
+    public Integer collectFee(Double fee){
+        System.out.println("Placeholder for collect fee");
+        return 0;
+    }
+}
