4ed9b47226d3e4aec101f4200adac2ca6f2358a2
Dwight J. Browne <djb@hard1.local>
2019-04-04 22:53:34 -0600

    Library incomplete

---
+++ HW4/src/main/java/edu/cuboulder/Library.java
@@ -0,0 +1,13 @@
+package edu.cuboulder;
+import java.util.List;
+
+public class Library {
+    private Inventory inventory;
+    private List<Person> users;
+    private Double lateFees;
+    private PersonDao personDao;
+    private InventoryDao inventoryDao;
+
+
+
+}
