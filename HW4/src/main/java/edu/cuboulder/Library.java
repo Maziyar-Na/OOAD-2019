582c477971e507de16bdc7996e07e7f462dd9656
Dwight J. Browne <djb@engr2gw-64-201-dhcp.int.colorado.edu>
2019-04-05 10:46:35 -0600

    minor changes

--- HW4/src/main/java/edu/cuboulder/Library.java
+++ HW4/src/main/java/edu/cuboulder/Library.java
@@ -9,5 +9,40 @@ public class Library {
     private InventoryDao inventoryDao;
 
 
+    Person addPerson(String name, String password, PersonType personType){
+        System.out.println("Placeholder for addperson");
+        return null;
+    }
+
+    Person getPerson(String name){
+        System.out.println("Placeholder for getPerson");
+        return null;
+    }
+
+    Person getPerson(Integer id){
+        System.out.println("Placeholder for getPerson");
+        return null;
+    }
+
+    Double getTotalLateFees(List<Person> patrons){
+        System.out.println("Placeholder for getTotalLateFees");
+        return 0.0;
+    }
+
+    Person modifyPerson(Person person){
+        System.out.println("modify person place holder");
+        return null;
+    }
+
+    Integer login(String userName, String password){
+        System.out.println("Placeholder for login");
+        return 0;
+    }
+
+    List<Person> getUsers(){
+        System.out.println("Placeholder for getUsers");
+        return null;
+    }
+
 
 }
