1023027e7ad2d13309f7e1e0d709851e2b6c766e
Dwight Browne <dwight@dwightjbrowne.com>
2019-04-25 07:44:31 -0600

    Should we delete this function??

--- Library/src/main/java/domain/DateSort.java
+++ Library/src/main/java/domain/DateSort.java
@@ -2,7 +2,7 @@ package domain;
 
 import java.util.List;
 
-public class DateSort implements SortInterface {
+public class DateSort implements SortInterface { /*TODO: remove */
     public List<Item> sort(List<Item> items) {
         System.out.print("Date Sort Placeholder");
         return null;
